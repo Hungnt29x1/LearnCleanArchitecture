d507465de97f31bf06781f1bf61dace712820dcb
Hungnt29x1 <hungnt29.x1@gmail.com>
2025-01-07 23:42:47 +0700

    init project

---
+++ CleanArchitecture.Infratructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infratructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
